97e0ed39bf647f74f1c4cc6cf56d1c1fe41c82c2
masayuki-tech <tennkuuno6@gmail.com>
2020-05-05 13:33:06 +0900

    中間テーブル作成
    複数のタグの取得に成功

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
+  root 'posts#index'
+  resources :posts, only: [:index, :new, :create, :show] do
+    collection do
+      get 'search'
+    end
+  end
+  
+  resources :genres, only: [:index, :show]
+  
 end
